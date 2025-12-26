7dd6d34bd33693b18eaa14e18845aa618ea9ff6a
TC-al <tc.alanliu@gmail.com>
2025-12-26 00:07:29 -0500

    sleepycowherding solution

--- src/Sleepycowherding.java
+++ src/Sleepycowherding.java
@@ -3,8 +3,6 @@ import java.io.*;
 
 public class Sleepycowherding {
     public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //PrintWriter pw = new PrintWriter(System.out);
         BufferedReader br = new BufferedReader(new FileReader("herding.in"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("herding.out")));
 
@@ -14,74 +12,12 @@ public class Sleepycowherding {
         for (int i = 0; i < 3; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
-        TreeSet<Integer> set2 = new TreeSet<Integer>();
-        for (int i = 0; i < 3; i++) {
-            set2.add((int) set.toArray()[i]);
-        }
-        pw.println(minimum(set));
-        pw.println(maximum(set2));
+
+        if ((int) set.toArray()[2] - (int) set.toArray()[1] == 1 && (int) set.toArray()[1] - (int) set.toArray()[0] == 1) pw.println(0);
+        else if ((int) set.toArray()[2] - (int) set.toArray()[1] == 2 || (int) set.toArray()[1] - (int) set.toArray()[0] == 2) pw.println(1);
+        else pw.println(2);
+
+        pw.println(Math.max((int) set.toArray()[2] - (int) set.toArray()[1], (int) set.toArray()[1] - (int) set.toArray()[0]) - 1);
         pw.close();
-    }
-
-    public static int minimum(TreeSet<Integer> set) {
-        int count = 0;
-        while ((int) set.toArray()[2] - (int) set.toArray()[1] != 1 || (int) set.toArray()[1] - (int) set.toArray()[0] != 1) {
-            if ((int) set.toArray()[2] - (int) set.toArray()[1] == 1) {
-                int tmp = (int) set.toArray()[1] - ((int) set.toArray()[1] - (int) set.toArray()[0]) / 2;
-                set.remove((int) set.toArray()[2]);
-                set.add(tmp);
-                count++;
-            } else if ((int) set.toArray()[1] - (int) set.toArray()[0] == 1) {
-                int tmp = (int) set.toArray()[1] + ((int) set.toArray()[2] - (int) set.toArray()[1]) / 2;
-                set.remove((int) set.toArray()[0]);
-                set.add(tmp);
-                count++;
-            } else {
-                if ((int) set.toArray()[2] - (int) set.toArray()[1] > (int) set.toArray()[1] - (int) set.toArray()[0]) {
-                    int tmp = (int) set.toArray()[1] - ((int) set.toArray()[1] - (int) set.toArray()[0]) / 2;
-                    set.remove((int) set.toArray()[2]);
-                    set.add(tmp);
-                    count++;
-                } else {
-                    int tmp = (int) set.toArray()[1] + ((int) set.toArray()[2] - (int) set.toArray()[1]) / 2;
-                    set.remove((int) set.toArray()[0]);
-                    set.add(tmp);
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public static int maximum(TreeSet<Integer> set) {
-        int count = 0;
-        while ((int) set.toArray()[2] - (int) set.toArray()[1] != 1 || (int) set.toArray()[1] - (int) set.toArray()[0] != 1) {
-            if ((int) set.toArray()[2] - (int) set.toArray()[1] == 1) {
-                int tmp = (int) set.toArray()[1] - 1;
-                set.remove((int) set.toArray()[2]);
-                set.add(tmp);
-                count++;
-            }
-            else if ((int) set.toArray()[1] - (int) set.toArray()[0] == 1) {
-                int tmp = (int) set.toArray()[1] + 1;
-                set.remove((int) set.toArray()[0]);
-                set.add(tmp);
-                count++;
-            }
-            else {
-                if ((int) set.toArray()[2] - (int) set.toArray()[1] > (int) set.toArray()[1] - (int) set.toArray()[0]) {
-                    int tmp = (int) set.toArray()[1] + ((int) set.toArray()[2] - (int) set.toArray()[1]) / 2;
-                    set.remove((int) set.toArray()[0]);
-                    set.add(tmp);
-                    count++;
-                } else {
-                    int tmp = (int) set.toArray()[1] - ((int) set.toArray()[1] - (int) set.toArray()[0]) / 2;
-                    set.remove((int) set.toArray()[2]);
-                    set.add(tmp);
-                    count++;
-                }
-            }
-        }
-        return count;
     }
 }
